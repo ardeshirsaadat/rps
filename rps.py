9f64223ab36d42cab5263431520543eca9779b19
ardeshirsaadat <ardeshirsaadat@gmail.com>
2020-09-08 11:28:25 -0400

    Keep score

--- rps.py
+++ rps.py
@@ -1,3 +1,4 @@
+import random
 moves = ['rock', 'paper', 'scissors']
 
 """The Player class is the parent class for all of the Players
@@ -12,21 +13,43 @@ class Player:
         pass
 
 
+class RandomPlayer(Player):
+    def move(self):
+        return random.choice(moves)
+
+
+
+
 def beats(one, two):
-    return ((one == 'rock' and two == 'scissors') or
-            (one == 'scissors' and two == 'paper') or
-            (one == 'paper' and two == 'rock'))
+    if ((one == 'rock' and two == 'scissors') or
+        (one == 'scissors' and two == 'paper') or
+        (one == 'paper' and two == 'rock')):
+        print("player 1 wins")
+        return "p1"
+    elif ((one == 'rock' and two == 'paper') or
+         (one == 'scissors' and two == 'rock') or
+         (one == 'paper' and two == 'scissors')):
+         print("player1 loses")
+         return "p2"   
+    else:
+        print("its a tie")
 
 
 class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
         self.p2 = p2
+        self.point_p1 = 0
+        self.point_p2 = 0
 
+        
     def play_round(self):
         move1 = self.p1.move()
         move2 = self.p2.move()
         print(f"Player 1: {move1}  Player 2: {move2}")
+        # beats(move1, move2)
+        self.keep_score(move1, move2)
+        print(f"player1 score: {self.point_p1} player2 score: {self.point_p2}")
         self.p1.learn(move1, move2)
         self.p2.learn(move2, move1)
 
@@ -36,8 +59,16 @@ class Game:
             print(f"Round {round}:")
             self.play_round()
         print("Game over!")
+    
+
+    def keep_score(self, one, two):
+        person_won = beats(one, two)
+        if person_won == "p1":
+            self.point_p1 +=1
+        elif person_won == "p2":
+            self.point_p2 +=1  
 
 
 if __name__ == '__main__':
-    game = Game(Player(), Player())
+    game = Game(RandomPlayer(), RandomPlayer())
     game.play_game()
